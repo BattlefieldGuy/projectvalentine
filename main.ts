922e5cade7fd8f8855d21223183d1edaee4f6381
BattlefieldGuy <144334879+BattlefieldGuy@users.noreply.github.com>
2025-02-17 11:56:27 +0100

    Button input work

--- main.ts
+++ main.ts
@@ -1,28 +1,28 @@
 //Vars
 let LoopState = 0
-let HeartLoop2Speed = 0
-let HeartLoop1Speed = 0
+let HeartLoop1Speed = 5
+let HeartLoop2Speed = 0.01
+
+LoopState = 0
 HeartLoop1Speed = 5
 HeartLoop2Speed = 0.01
-LoopState = 1
 //start
 basic.showString("LOVE YOU")
 // state check
 basic.forever(function () {
-    if (LoopState == 1) {
+    if (LoopState == 0) {
         PlayHeartLoop()
-    } else if (LoopState == 2) {
+    } else if (LoopState == 1) {
         PlayHeartLoop2()
-    } else if (LoopState == 3){
+    } else if (LoopState == 2) {
         ButtonASequence()
     }
+    if (input.buttonIsPressed(Button.A)) {
+        LoopState = 2
+    }
 })
-//input
-input.buttonIsPressed(Button.A);{
-    LoopState = 3;
-}
 // HeartLoops
-function PlayHeartLoop2 () {
+function PlayHeartLoop2() {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -148,110 +148,111 @@ function PlayHeartLoop2 () {
         # # # # #
         # # # # #
         . # # # .
+        . . # . .
+        `)
+    LoopState = 0
+}
+function PlayHeartLoop() {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(HeartLoop1Speed)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
         . . # . .
         `)
     LoopState = 1
 }
-function PlayHeartLoop () {
+//button sequences
+function ButtonASequence() {
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . # . # .
-        # . # . #
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . # . # .
-        # . # . #
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(HeartLoop1Speed)
-    basic.showLeds(`
-        . # . # .
-        # . # . #
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    LoopState = 2
-}
-function ButtonASequence(){
-basic.showLeds(`
     . . . . .
     . . . . .
     . . # . .
